8e99e76082c18d621108051cfcd71943ebe94b66
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-08-20 17:45:05 +0800

    use default cors configs

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,19 +1,12 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000', 'http://localhost:3666', 'http://127.0.0.1:3666']
-    }
-  },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+]
